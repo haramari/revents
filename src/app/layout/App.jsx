96915e93a591ab38acc4bec1c68cf4b7a1db4d2a
haramari <haramari.rps@gmail.com>
2019-11-26 23:54:36 +0900

    Initial Commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render(){
+      return (
+    <div>
+      <h1>Re-vents</h1>
+      <h2>Tis' but a scratch!</h2>
+      <h3>超ウィザード級ハッカー：Haruto Aki</h3>
+      <h4>脳の構造をまとめましょう。</h4>
+    </div>
+    );
+  }
+}
+
+export default App;
